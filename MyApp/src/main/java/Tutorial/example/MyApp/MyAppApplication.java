3656307179a1d7c0a36f7448a322d49db0e4dbbf
Milan Shahi <milanshahi884@gmail.com>
2025-03-16 14:38:47 -0400

    Changes to Master Branch

--- MyApp/src/main/java/Tutorial/example/MyApp/MyAppApplication.java
+++ MyApp/src/main/java/Tutorial/example/MyApp/MyAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude={org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class MyAppApplication {
 
+	//Changes to Master
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
 	}
